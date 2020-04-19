8e79b31d74f079f854b476363e671e8316158c72
Geiler20182 <44622578+Geiler20182@users.noreply.github.com>
2020-04-19 17:09:28 -0500

    Cambio 005 -> Factura

--- Reto1/src/main/scala/Test/Test.scala
+++ Reto1/src/main/scala/Test/Test.scala
@@ -4,16 +4,22 @@ import Usuarios._
 import Netflix._
 import Complemento._
 import Planes._
-
+import java.time._
+import java.util.Calendar
 object Test {
 
     def main(args: Array[String]) : Unit = {
 
         /* Test */ 
-        
         var nuevo_plan : Plan = new Plan
         var nuevo_usuario : UsuarioAdmin = new UsuarioAdmin("Geiler@org", "%$#", "Geiler")
+        var nueva_factura : Factura = new Factura
+        println("=======================")
+
+        println("=======================")
 
 
     }   
+
+
 }
--- Reto1/src/main/scala/Usuarios/Usuario.scala
+++ Reto1/src/main/scala/Usuarios/Usuario.scala
@@ -2,10 +2,11 @@ package Usuarios
 
 import Complemento._
 import Planes._
+import scala.util.Random
 
 class Usuario {
     
-
+    protected var _id_usario : String = generarID()
     protected var _nombre :  String = _
     protected var _correo :  String = _
     protected var _contrasena : String = _
@@ -16,7 +17,13 @@ class Usuario {
     def getNombre() : String = _nombre
     def getCorreo() : String = _correo
     def getContrasena() : String = _contrasena
-    
+
+    def generarID() : String = {
+
+        var id : String = Random.alphanumeric.take(10).mkString("")
+        
+        return id
+    }    
     
 
 }
